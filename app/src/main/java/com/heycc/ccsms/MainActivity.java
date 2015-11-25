3da8792f92e45fc318aadeead1a3159e18931eb7
heycc <cc.changchun@gmail.com>
2015-11-25 23:43:31 +0800

    simple listview

--- app/src/main/java/com/heycc/ccsms/MainActivity.java
+++ app/src/main/java/com/heycc/ccsms/MainActivity.java
@@ -5,11 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //Define ListView
     ListView mListView;
+    String[] countries = new String[]{"China", "Turkey", "United States"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         mListView = (ListView) findViewById(R.id.list);
+        mListView.setAdapter(new ArrayAdapter<>(this, R.layout.row_list_main, countries));
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> av, View v, int p, long id) {
+                String item = (String) av.getItemAtPosition(p);
+                Toast.makeText(getApplicationContext(), item + " selected", Toast.LENGTH_LONG).show();
+            }
+        });
         /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
